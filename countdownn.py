539260c281879dc59d58672cba38aec96dd9cb40
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-16 14:38:58 -0400

    Updated from Brython Server: 10/16/2017 2:38:57 PM

--- countdownn.py
+++ countdownn.py
@@ -1,3 +1,12 @@
 #Pedro Gallino
 #10/16/17
+#countdownn.py - counts down from a number
 
+def countdown(num1):
+    i = num1
+    while i>0:
+        print(i)
+        i -=1
+    print('BOOM!')
+
+countdown(5)
