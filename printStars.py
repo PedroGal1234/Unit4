ec666716a920c9f374ad653a82ad2ec83c2a778c
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-16 14:34:57 -0400

    Updated from Brython Server: 10/16/2017 2:34:55 PM

--- printStars.py
+++ printStars.py
@@ -1,1 +1,13 @@
+#Pedro Gallino
+#10/16/17
+#printStars.py - print lines of stars equal to an intiger
 
+def stars(num1):
+    i = 1
+    space = num1
+    while i<num1+1:
+        print(' '*space,'*'*i)
+        i += 2
+        space = space-1
+
+stars(30)
