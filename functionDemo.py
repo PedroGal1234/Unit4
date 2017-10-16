6911789b9a0290341ea36ed3c338909d3558b56d
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-16 14:27:13 -0400

    Updated from Brython Server: 10/16/2017 2:27:11 PM

--- functionDemo.py
+++ functionDemo.py
@@ -4,3 +4,22 @@
 
 def hw():
     print ('Hellow,world!')
+    
+def bigger(num1, num2): #prints which function is bigger
+    if num1>num2:
+        print(num1)
+    else:
+        print(num2)
+def slope(x1, y1, x2, y2):
+    print((y2-y1)/(x2-x1))
+
+
+
+#hw() #runs function
+#bigger(13,27) #test 1
+#bigger(-10,-15) #test 2
+#bigger('Smeds', 'Clay')
+slope(1,2,3,4,)
+
+
+    
