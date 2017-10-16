9bec12434abe4c2ec73114860c63f9416666ca2c
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-16 14:30:35 -0400

    Updated from Brython Server: 10/16/2017 2:30:33 PM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,9 @@
+#Pedro Gallino
+#10/16/17
+#tells you the perimiter of a rectnagle
 
+def rectangle(num1,num2):
+    print('The area is',num1*num2)
+    print('The perimeter is:',num1*2+num2*2)
+
+rectangle(3,4)
