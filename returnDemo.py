a4a0328d962dd368300866d2f7ecb2f9f743c769
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-18 13:38:33 -0400

    Updated from Brython Server: 10/18/2017 1:38:33 PM

--- returnDemo.py
+++ returnDemo.py
@@ -1,1 +1,17 @@
+#Pedro Gallino
+#10/18/17
+#returnDemo.py - learning return
 
+from random import randint
+
+def randevenint(low,high):
+    num = randint(low,high)
+    while num%2 == 1:
+        num = randint(low,high)
+    return num
+
+r1 = randevenint(1,100)
+r2 = randevenint(1,100)
+r3 = randevenint(1,100)
+
+print(r1,r2,r3)
