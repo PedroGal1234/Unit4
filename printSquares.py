b325c9604a5a84f6906387949c7ec83cc7da42ee
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-18 13:47:41 -0400

    Updated from Brython Server: 10/18/2017 1:47:41 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,15 @@
+#Pedro Gallino
+#10/18/17
+#/printSquares.py - prints a patern of squares
 
+def printSquares(num1,num2):
+    for i in range(0,num2):
+        if i == 0:
+            print('+--'*num1+('+'))
+            print('|  '*num1+('|'))
+            print('+--'*num1+('+'))
+        else:
+            print('|  '*num1+('|'))
+            print('+--'*num1+('+'))
+
+printSquares(4,4)
