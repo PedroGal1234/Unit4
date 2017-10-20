032a0147c2abd940ba21ccbc0097bf114b4202d3
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-20 14:54:23 -0400

    Updated from Brython Server: 10/20/2017 2:54:23 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,15 @@
+#Pedro Galllino
+#10/20/17
+#stringUnion.py - unites two strings
 
+total = ''
+def stringUnion(word1,word2):
+    for ch in word1:
+        if ch not in total:
+            total = str(total)+str(ch)
+    for ch in word2:
+        if ch not in total:
+            total = str(total)+str(ch)
+
+stringUnion('big','bro')
+    
