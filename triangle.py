79fd4c0afbd2d26e59a921998bcf4f7efaf9b12b
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-23 14:55:10 -0400

    Updated from Brython Server: 10/23/2017 2:55:09 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,17 @@
+#Pedro Gallino
+#10/23/17
+#triangle.py tells you the area of a triangle
+
+from math import sqrt
+def distance(x1,y1,x2,y2):
+    return sqrt(((x2-x1)**2)+((y2-y1)**2))
+
+x1 = int(input('X1 = ')
+y1 = int(input('y1 = ')
+x2 = int(input('X2 = ')
+y2 = int(input('Y2 = ')
+x3 = int(input('X3 = ')
+y3 = int(input('Y3 = ')
+
+
 
