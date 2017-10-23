865e22735bb20104e28cc2557d7fca2f4c0cc11e
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-23 14:35:27 -0400

    Updated from Brython Server: 10/23/2017 2:35:27 PM

--- warmUp11.py
+++ warmUp11.py
@@ -1,1 +1,13 @@
+#Pedro Gallino
+#10/23/17
+#warmUp11.py - does prime functions
 
+def isPrime(n):
+    x = 2
+    while x < n:
+        if n%x == 0:
+            return False
+        n += 1
+    return True
+
+print(isPrime(5))
