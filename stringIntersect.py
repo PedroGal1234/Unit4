01611692cf4ffefb4c283d53b47fe07161d968a9
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-23 14:51:23 -0400

    Updated from Brython Server: 10/23/2017 2:51:22 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,14 @@
+#Pedro Gallino
+#10/23/17
+#stringIntersect.py - tells you letters that apear in both words
+
+def stringIntersect(word1, word2):
+    total = " "
+    for ch in word1:
+        if not ch in total:
+            if ch in word1 and ch in word2:
+                total += ch
+    return total
+
+print(stringIntersect("mississippi", "pennsylvania"))
 
