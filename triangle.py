bd9d37fc8456ea19fc6deb4e4aa29788ff9b7ec2
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-25 13:50:17 -0400

    Updated from Brython Server: 10/25/2017 1:50:17 PM

--- triangle.py
+++ triangle.py
@@ -13,9 +13,9 @@ y2 = int(input('Y2 = '))
 x3 = int(input('X3 = '))
 y3 = int(input('Y3 = '))
 
-a = distance(x1,y1,y2,y2)
-b = distance(x1,y1,y3,y3)
-c = distance(x2,y2,y3,y3)
+a = distance(x1,y1,x2,y2)
+b = distance(x1,y1,x3,y3)
+c = distance(x2,y2,x3,y3)
 s = .5*(a+b+c)
 
 print(sqrt(s*(s-a)*(s-b)*(s-c)))
