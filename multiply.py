0c1fb46be0364f61c99bbbbc0abc01577d9e1508
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-25 13:50:55 -0400

    Updated from Brython Server: 10/25/2017 1:50:54 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,20 @@
+#Pedro Gallino
+#10/25/17
+#multiply.py - multiplying game
+
+from random import randint
+
+numCorrect = 0
+
+while numCorrect < 5:
+    num1 = randint(0,12)
+    num2 = randint(0,12)
+    question = 'What is '+ str(num1) + 'x' + str(num2) + '?'
+    awnser = int(input(question))
+    if num1*num2 == awnser:
+        print('Correct!')
+        numCorrect += 1
+    else:
+        print('Sorry thats Incorrect')
+print("Congratiolations! You Win!")
 
