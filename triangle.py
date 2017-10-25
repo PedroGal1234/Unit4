a7f392524f60943319de38fddc1311586e01fc16
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-25 13:40:19 -0400

    Updated from Brython Server: 10/25/2017 1:40:19 PM

--- triangle.py
+++ triangle.py
@@ -6,12 +6,22 @@ from math import sqrt
 def distance(x1,y1,x2,y2):
     return sqrt(((x2-x1)**2)+((y2-y1)**2))
 
-x1 = int(input('X1 = ')
-y1 = int(input('y1 = ')
-x2 = int(input('X2 = ')
-y2 = int(input('Y2 = ')
-x3 = int(input('X3 = ')
-y3 = int(input('Y3 = ')
+x1 = int(input('X1 = '))
+y1 = int(input('y1 = '))
+x2 = int(input('X2 = '))
+y2 = int(input('Y2 = '))
+x3 = int(input('X3 = '))
+y3 = int(input('Y3 = '))
+
+a = distance(x1,y1,y2,y2)
+b = distance(x1,y1,y3,y3)
+c = distance(x2,y2,y3,y3)
+s = .5*(a+b+c)
+
+print(sqrt(s*(s-a)*(s-b)*(s-c)))
+
+
+
 
 
 
