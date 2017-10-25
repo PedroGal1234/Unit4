d9687f2dbc761069cb966bc0eafbc86715deb46d
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-25 17:34:48 -0400

    Updated from Brython Server: 10/25/2017 5:34:47 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -1,1 +1,40 @@
+#Pedro Gallino
+#10/25/17
+#bouncingBall.py - displays a bouncing ball
+
+from ggame import *
+
+diameter = 50
+
+def moveBall():
+    ball.x = ball.x +1
+    ball.y = ball.y + 1
+    data['frames'] = 0
+
+def bounceBall():
+    ball.x = ball.x - 1
+    ball.y = ball.y - 1
+    data['frames'] = 0
+
+
+def step():
+    data['frames'] += 1
+    if data['frames'] == 1:
+        if ball.x+50 == 523 and ball.y+50 == 1012:
+            bounceBall()
+        else:
+            moveBall()
+    
+
+
+data = {}
+data['frames'] = 0
+    
+red = Color(0xFF00, 1)
+
+circle = CircleAsset(diameter,LineStyle(10,red),red)
+
+ball = Sprite(circle,(50,50))
+
+App().run(step)
 
