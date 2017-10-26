9dd49ba8cf239082b5419c4b6b4b95039a4435db
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-26 14:12:04 -0400

    Updated from Brython Server: 10/26/2017 2:12:03 PM

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,1 +1,12 @@
+#Pedro Gallino
+#10/26/17
+#recursionDemo.py - learning about recursion: a recursive version of the countdown function
 
+def countdown(n):
+    if n == 0:
+        print('Boom!')
+    else:
+        print(n)
+        countdown(n-1)
+
+countdown(5)
