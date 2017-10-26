55734a6875b411acb771f61a57fe317e4296dfe1
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-26 14:33:40 -0400

    Updated from Brython Server: 10/26/2017 2:33:39 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -4,10 +4,10 @@
 
 from ggame import *
 
-diameter = 50
+radius = 50
 
 def moveBall():
-    ball.x = ball.x +data['xd']
+    ball.x = ball.x -data['xd']
     ball.y = ball.y +data['yd']
 
 def moveX():
@@ -18,23 +18,22 @@ def movey():
 
 
 def step():
-    if ball.x+50 > 1024: 
-        changex()
-    if ball.y+50 > 523:
-        changey()
-    else:
-        moveBall()
+    if ball.x+50 >= 1015 or ball.x-50 <= 0: 
+        movex()
+    if ball.y+50 >= 523 or ball.y-50 <= 0:
+        movey()
+    moveBall()
 
+if __name__ == "__main__":    
+    red = Color(0xFF00, 1)
     
-red = Color(0xFF00, 1)
-
-circle = CircleAsset(diameter,LineStyle(0,red),red)
-
-ball = Sprite(circle,(50,50))
-
-data = {}
-data["xd"] = 5.23
-data['yd'] = 5.23
-
-App().run(step)
+    circle = CircleAsset(radius,LineStyle(0,red),red)
+    
+    ball = Sprite(circle,(100,100))
+    
+    data = {}
+    data["xd"] = 5.23
+    data['yd'] = 5.23
+    
+    App().run(step)
 
