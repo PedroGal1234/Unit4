03e264249c4116276dd351222d527e0612135a7a
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-27 14:02:55 -0400

    Updated from Brython Server: 10/27/2017 2:02:55 PM

--- factorial.py
+++ factorial.py
@@ -1,1 +1,17 @@
+#Pedro Gallino
+#10/27/17
+#factorial.py - finds factorial using recursion
+
+def factorial(n):
+    if n == 1:
+        return(n)
+    else:
+        return n*factorial(n-1)
+        
+        
+print(factorial(5))
+
+
+
+
 
