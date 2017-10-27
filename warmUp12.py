73a35926e3daafc01cef51a37d0d7357be5fb64a
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-27 14:25:19 -0400

    Updated from Brython Server: 10/27/2017 2:25:18 PM

--- warmUp12.py
+++ warmUp12.py
@@ -1,1 +1,13 @@
+#Pedro Gallino
+#10/27/17
+#warmUp12.py - finds the greatest common factor of two numbers
 
+def GCF(x,y):
+    n = y
+    while n>0: 
+        if x%n == 0 and y%n == 0:
+            return n
+        else:
+            n -= 1
+    
+print(GCF(20,30))
