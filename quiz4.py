c7f50da448d0b19804c673b5a8f376d0a48fb471
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-10-30 14:32:11 -0400

    Updated from Brython Server: 10/30/2017 2:32:11 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,30 @@
+#Pedro Gallino
+#10/30/17
+#quiz4.py - my fourth proggraming quiz on functions
 
+def csia():
+    for i in range(0,5):
+        print('computer science is awesome')
+
+def average(x,y,z):
+    return ((x+y+z)/3)
+
+def lastLetter(word):
+    end = len(word)
+    times = 0
+    for ch in word:
+        times += 1
+        if times == end:
+            return ch
+
+def same(a,b):
+    if a == b:
+        return True
+    else:
+        return False
+        
+
+csia()
+print(average(1,2,3))
+print(lastLetter('Smedinghoff'))
+print(same(2*3,7-1))
